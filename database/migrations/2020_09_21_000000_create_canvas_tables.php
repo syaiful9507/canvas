f71d1bd348ae1d9fbbdfcd164bb94ae255935f49
Todd Austin <austin.todd.j@gmail.com>
2022-07-05 14:27:37 -0500

    use anonymous migrations

--- database/migrations/2020_09_21_000000_create_canvas_tables.php
+++ database/migrations/2020_09_21_000000_create_canvas_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCanvasTables extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -116,4 +116,4 @@ class CreateCanvasTables extends Migration
         Schema::dropIfExists('canvas_visits');
         Schema::dropIfExists('canvas_users');
     }
-}
+};
