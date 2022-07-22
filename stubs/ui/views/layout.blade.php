1eaab5c9fc99c6fb99559b61792956afd32ce465
Todd Austin <austin.todd.j@gmail.com>
2022-07-22 11:33:56 -0500

    refactoring ui to blade

---
+++ stubs/ui/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="https://cdn.tailwindcss.com?plugins=typography"></script>
+    <title>Canvas UI</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
+
