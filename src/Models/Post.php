81a98347f00ac4ae776087a66ac08ab36f9f809b
Todd Austin <austin.todd.j@gmail.com>
2022-11-11 16:18:24 -0600

    Apply fixes from StyleCI (#1164)

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -6,7 +6,6 @@ namespace Canvas\Models;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
